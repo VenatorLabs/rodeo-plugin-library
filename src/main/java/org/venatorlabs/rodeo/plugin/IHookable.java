cc6d10184a44441cb83034e473434aa8b0e5eabc
codejedi365 <codejedi365@gmail.com>
2023-06-04 15:34:18 -0400

    feat(plugin): create base interface for rodeo-plugins
    BREAKING CHANGES: initial release

---
+++ src/main/java/org/venatorlabs/rodeo/plugin/IHookable.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023 Venator Labs, Inc. All Rights Reserved.
+ * This code is licensed under the MIT License.
+ *
+ * For license terms, see LICENSE
+ */
+
+package org.venatorlabs.rodeo.plugin;
+
+import java.util.ArrayList;
+import java.util.Map;
+import org.venatorlabs.rodeo.types.INode;
+import org.venatorlabs.rodeo.types.IRelationship;
+
+/**
+ * Interface for hooking and manipulating documents, nodes, and relationships in the pipeline.
+ */
+public interface IHookable {
+    public boolean isDocHook();
+
+    public void processDoc(Map<String, Object> doc);
+
+    public boolean isNodeHook();
+
+    public void processNodes(ArrayList<INode> nodes);
+
+    public boolean isRelationshipHook();
+
+    public void processRelationships(ArrayList<IRelationship> rels);
+}
---
+++ src/main/java/org/venatorlabs/rodeo/plugin/RodeoPlugin.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 Venator Labs, Inc. All Rights Reserved.
+ * This code is licensed under the MIT License.
+ *
+ * For license terms, see LICENSE
+ */
+
+package org.venatorlabs.rodeo.plugin;
+
+/**
+ * Base interface for creating a Rodeo plugin.
+ */
+public interface RodeoPlugin extends IHookable {
+    public String getName();
+
+    public String getDescription();
+}
